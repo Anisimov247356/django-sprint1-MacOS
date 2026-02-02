faf2311431a29d1205ec4a250d931be9f9157794
Анисимов Вячеслав <Anisimov-Vyacheslav@yandex.ru>
2026-02-02 21:21:44 +0300

    Django-sprint-MacOS

--- blogicum/blog/apps.py
+++ blogicum/blog/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
-
     """Конфигурация приложения 'blog'."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
